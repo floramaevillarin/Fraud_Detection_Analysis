9f4fea520993527e8e4acb69a57b9559d5010bab
Haldo Somoza <haldo_somoza@yahoo.com>
2023-11-24 01:50:03 -0500

    Updating script to call model.

--- fraud-detection-demo/prediction.py
+++ fraud-detection-demo/prediction.py
@@ -1,82 +1,175 @@
-import sklearn
+import json
 import joblib
+import numpy  as np
 import pandas as pd
 
-from flask import Flask, request, jsonify
+from flask import jsonify
 
 #=== === === === === ===
 
+RESULT           = "result"
+RESULT_FRAUD     = "FRAUD"
+RESULT_NOT_FRAUD = "NOT-FRAUD"
+RESULT_UNKNOWN   = "UNKNOWN"
+RESULT_ERROR     = "ERROR"
+
 def post(request):
     try:
-        # Get the JSON data from the request
+        # getting the JSON data from the request
         data_json = request.json
 
-        # If come all params ...
-        validate(data_json)
+        # validating the presence of parameters
+        names_list, fields_dict = validate(data_json)
 
-        homo_json = homologate(data_json)
+        # homologating values
+        homo_json = homologate(names_list, fields_dict, data_json)
         
-        # Get the prediction 
+        # getting the prediction 
         result = prediction(homo_json)
         
-        save_data(data_json, result)
+        # saging the data
+        save_data(names_list, data_json, result)
 
-        response = {'result': result}
+        response = {RESULT: result}
         return jsonify(response)
 
     except Exception as ex:
         # throwing the error in result
-        error_msg = 'ERROR: '+ex.args[0]
-        save_data(data_json, error_msg)
+        error_msg = RESULT_ERROR+': '+str(ex.args[0])
+        save_data(names_list, data_json, error_msg)
 
-        response  = {'result': error_msg}
+        response  = {RESULT: error_msg}
         return jsonify(response), 400  # 400 Bad Request
 
 #=== === === === === ===
 
-def validate(data_json):
-    # If come all params ...
-    if not ('param01' in data_json and 'param02' in data_json and 'param03' in data_json and 'param04' in data_json and 'param05' in data_json):
-        # throwing the error in result
-        raise Exception("Parameters required")
+def get_field_by_name(fields_list, name):
+    for field in fields_list:
+        if field.get("name") == name:
+            return field
+    return None
+
+def load_fields():
+    fields_path = "data/fields.json"
+    with open(fields_path, "r") as json_file:
+        fields_list = json.load(json_file)
+    
+    names_list = [field["name"] for field in fields_list]
+    fields_dict = { }
+    for name in names_list:
+        fields_dict[name] = get_field_by_name(fields_list, name)
+
+    return names_list, fields_dict
 
 #=== === === === === ===
 
-def homologate(data_json):
-    param01 = int(data_json['param01'])
-    param02 = int(data_json['param02'])
-    param03 = int(data_json['param03'])
-    param04 = int(data_json['param04'])
-    param05 = int(data_json['param05'])
+def is_null_or_empty(obj):
+    if obj is None:
+        return True
+    elif isinstance(obj, str) and not obj.strip():
+        return True
+    elif isinstance(obj, (list, tuple, dict, set)) and not obj:
+        return True
+    else:
+        return False
+
+def validate(data_json):
+    names_list, fields_dict = load_fields()
+
+    params_unknown_list = [ ]
+    for key, value in data_json.items():
+        if not key in names_list:
+            params_unknown_list.append(key)
+    if len(params_unknown_list) > 0:
+        raise Exception("Parameters unknown: "+", ".join(params_unknown_list))
+
+    params_missed_list = [ ]
+    for name in names_list:
+        if fields_dict.get(name)["required"] in (True, "true"):
+            if not name in data_json:
+                params_missed_list.append(name)
+            elif is_null_or_empty(data_json[name]):
+               params_missed_list.append(name)
+    if len(params_missed_list) > 0:
+        raise Exception("Parameters required: "+", ".join(params_missed_list))
     
-    homo_json = {
-        "a": [param01],  # 1
-        "b": [param02],  # 11
-        "c": [param03],  # 21
-        "d": [param04],  # 31
-        "e": [param05]   # 41
-    }
+    return names_list, fields_dict
+
+#=== === === === === ===
+
+def homologate(names_list, fields_dict, data_json):
+    homo_json = { }
+    for name in names_list:
+        # getting the final name to send to the model
+        homo_name = name
+        if 'encoded_name' in fields_dict[name]: 
+            homo_name = fields_dict[name]['encoded_name']
+        print(homo_name)
+
+        # if element is not sended, set null and continue
+        if not name in data_json:
+            homo_json[homo_name] = np.nan
+            continue
+        
+        # if the value is null or empty, set null and continue
+        value = data_json[name]
+        if is_null_or_empty(value):
+            homo_json[homo_name] = np.nan
+            continue                        
+
+        # if there are not encoded list, set the value as float and continue
+        if not 'encoded_list' in fields_dict[name]:
+            homo_json[homo_name] = float(value)
+            continue
+        
+        # getting the encoded list
+        values_dict = { }
+        list_filename = "models/"+fields_dict[name]['encoded_list']
+        with open(list_filename, "r") as json_file:
+            values_dict = json.load(json_file)
+
+        # if value is found, set the value as float and continue
+        if value in values_dict:
+            homo_json[homo_name] = float(values_dict[value])
+            continue
+        elif str(value)+".0" in values_dict:
+            homo_json[homo_name] = float(values_dict[str(value)+".0"])
+            continue
+        
+        # reached here, nothing more to do, set null
+        homo_json[homo_name] = np.nan
+        
+    print("data_json => ", data_json)
+    print("homo_json => ", homo_json)
+
+    # returning the homologated JSON
     return homo_json
 
 #=== === === === === ===
 
 def prediction(data_json):
-    sklearn_version = sklearn.__version__
-    filename = f'models/model_joblib_{sklearn_version}.pkl'
-
-    model_loaded = joblib.load(filename)
+    model_filename = 'models/model_xgbv3_joblib.pkl'
+    model_loaded = joblib.load(model_filename)
     y_pred = model_loaded.predict(pd.DataFrame(data_json))
-    return str(y_pred[0])
+    result = str(y_pred[0])
+    return RESULT_FRAUD if result == "1" else RESULT_NOT_FRAUD if result == "0" else RESULT_UNKNOWN
 
 #=== === === === === ===
 
-def save_data(data_json, result):
-    data_file_path = "data/data.csv"
-    with open(data_file_path, 'a') as file:
-        new_line = json_csv_line(data_json) + ", " + result
+def save_data(names_list, data_json, result):
+    data_filename = "data/data.csv"
+    with open(data_filename, 'a') as file:
+        new_line = json_csv_line(names_list, data_json) + ", " + result
         file.write('\n'+new_line)
 
-def json_csv_line(data_json):
-    return ', '.join([f'{value}' for key, value in data_json.items()])
+def json_csv_line(names_list, data_json):
+    values_list = [ ]
+    for name in names_list:
+        if name in data_json:
+            if is_null_or_empty(data_json[name]):
+                values_list.append("")
+            else: values_list.append(data_json[name].replace(',',';'))
+        else: values_list.append("")
+    return ', '.join(values_list)
 
 #=== === === === === ===
